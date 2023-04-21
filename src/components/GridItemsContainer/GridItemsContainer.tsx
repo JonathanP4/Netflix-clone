0c3d4241ec936ebee62714f623cff02cd90f72ef
Jonathan <jonathanpetersen2016@gmail.com>
2023-04-21 16:10:16 -0300

    Todo active states

--- src/components/GridItemsContainer/GridItemsContainer.tsx
+++ src/components/GridItemsContainer/GridItemsContainer.tsx
@@ -1,27 +1,31 @@
 import GridItem from "../GridItem/GridItem";
 import classes from "./GridItemsContainer.module.css";
+import enjoyOnTv from "../../assets/img/enjoyOnTv.png";
+import watchEverywhere from "../../assets/img/watchEverywhere.png";
+import kids from "../../assets/img/netflixKids.png";
+import download from "../../assets/img/download.png";
 
 const itemsData = [
   {
-    imgSrc: "/src/assets/img/enjoyOntv.png",
+    imgSrc: enjoyOnTv,
     alt: "enjoy_on_your_Tv",
     title: "Enjoy on your TV.",
     text: "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
   },
   {
-    imgSrc: "/src/assets/img/watchEverywhere.png",
+    imgSrc: watchEverywhere,
     alt: "watch_everywhere",
     title: "Watch everywhere.",
     text: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.",
   },
   {
-    imgSrc: "/src/assets/img/netflixKids.png",
+    imgSrc: kids,
     alt: "create_profiles_for_kids",
     title: "Create profiles for kids.",
     text: "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
   },
   {
-    imgSrc: "/src/assets/img/download.png",
+    imgSrc: download,
     alt: "watch_offline",
     title: "Download your shows to watch offline.",
     text: "Only available on ad-free plans.",
